f7c107b8e4050e1e642a30023c59f3b81ab11df9
SarooshAshraf <sarooshashraf@gmail.com>
2025-10-02 02:36:49 -0400

    fixed routing

--- next.config.js
+++ next.config.js
@@ -9,9 +9,11 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: { typedRoutes: true },
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  trailingSlash: true,
   // If you later use next/image with static export, you'll need this:
   images: { unoptimized: true },
 };
 
 module.exports = withMDX(nextConfig);
+
 
